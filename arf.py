b6a1c3670952c88d26516bfd11ae3cb9e0090fd4
Dan Meliza <dan@meliza.org>
2021-02-10 08:41:51 -0500

    is_time_series() works if "units" attr is missing

--- arf.py
+++ arf.py
@@ -433,11 +433,9 @@ def count_children(obj, type=None):
 
 def is_time_series(dset):
     """Return True if dset is a sampled time series (units are not time)"""
-    return (
-        not is_marked_pointproc(dset)
-        and "sampling_rate" in dset.attrs
-        and dset.attrs["units"] not in ("s", "samples")
-    )
+    return (not is_marked_pointproc(dset)
+            and 'sampling_rate' in dset.attrs
+            and dset.attrs.get("units", None) not in ('s', 'samples'))
 
 
 def is_marked_pointproc(dset):
